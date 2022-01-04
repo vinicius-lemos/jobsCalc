ebfbed162dbed644c5ba91e8ec4065d07e071449
Vinicius Lemos <viniciuslemos@alu.ufc.br>
2022-01-03 22:20:29 -0300

    SQLite

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const server = express()
 const routes = require("./routes")
+const path = require("path")
 
 server.set('view engine', 'ejs') // visualizar arquivos ejs
+
+// mudar a localização da pasta "views"
+server.set('views', path.join(__dirname, 'views'))
 
 // habilitar arquivos statics
 server.use(express.static("public"))
