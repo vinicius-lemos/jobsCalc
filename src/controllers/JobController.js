60c6e1aba5b3b0748f1ba139d79d556453342f3c
Vinicius Lemos <viniciuslemos@alu.ufc.br>
2022-01-03 22:20:54 -0300

    SQLite

---
+++ src/controllers/JobController.js
@@ -0,0 +1,60 @@
+const Job = require('../model/Job')
+const JobUtils = require('../utils/JobUtils')
+const Profile = require('../model/Profile')
+
+module.exports = {
+    create(req, res) {
+      return res.render("job")
+    },
+
+    async save(req, res) {
+        // req.body retorna um objeto com os dados do form
+        await Job.create({
+            name: req.body.name,
+            "daily-hours": req.body["daily-hours"],
+            "total-hours": req.body["total-hours"],
+            created_at: Date.now() // atribuindo data atual
+        })
+
+        return res.redirect('/') // redireciona para a rota '/'
+    },
+
+    async show(req, res) {
+        const jobs = await Job.get()
+        const profile = await Profile.get()
+
+        const jobId = req.params.id
+
+        const job = jobs.find(job => Number(job.id) === Number(jobId)) // find busca um valor no array e retorna o elemento do array que possui esse valor
+
+        if(!job) {
+            return res.send('Job not found!')
+        }
+
+        job.budget = JobUtils.calculateBudget(job, profile["value-hour"])
+
+        return res.render("job-edit", { job })
+    },
+
+    async update(req, res) {
+        const jobId = req.params.id
+
+        const updatedJob = {
+            name: req.body.name,
+            "total-hours": req.body["total-hours"],
+            "daily-hours": req.body["daily-hours"]
+        }
+
+        await Job.update(jobId, updatedJob)
+
+        return res.redirect('/job/' + jobId)
+    },
+
+    async delete(req, res) {
+        const jobId = req.params.id
+
+        await Job.delete(jobId)
+
+        return res.redirect('/')
+    }
+}
